67d3c10b5b4748d3b661426326d434f297dc863c
Umee7 <umeshmessi23@gmail.com>
2025-03-28 01:41:57 +0530

    Initialized the app and downloaded required dependencies

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = 9001;
+
+const app = express();
+
+app.listen(PORT, ()=> console.log(`port ${PORT} is running...`));
+
+
+
+
+
+
+
+
+
